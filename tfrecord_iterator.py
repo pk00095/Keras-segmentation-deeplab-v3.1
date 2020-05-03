db9fd98096811a290d6fb6171278bee4df99c073
pk00095 <pk00095@gmail.com>
2020-05-03 11:44:18 +0530

    proper shape for y_true

--- tfrecord_iterator.py
+++ tfrecord_iterator.py
@@ -30,6 +30,9 @@ def parse_tfrecords(filenames, height, width, num_classes, batch_size=32):
         mask = tf.image.resize_images(mask,(height, width))
         mask.set_shape([height, width,1])
 
+        mask = tf.keras.backend.one_hot(tf.squeeze(tf.dtypes.cast(mask, tf.int32)), num_classes) #[:,:,:-1]
+        mask = tf.reshape(y_true, shape=(height*width, num_classes))
+
         return tf.cast(image, tf.float32), mask
     
     dataset = tf.data.TFRecordDataset(filenames=filenames)
